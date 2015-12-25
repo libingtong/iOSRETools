b6ba3efe3271eb212480397ba7eb863a25a38d19
Naville Zhang <zhang.naville@me.com>
2015-12-25 13:58:17 +0000

    THEOS

--- Installer.sh
+++ Installer.sh
@@ -2,3 +2,12 @@
 echo "Installing MacPorts"
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew install dpkg
+echo "export THEOS=\"/opt/theos\">>~/.bash_profile
+sudo git clone git://github.com/DHowett/theos.git $THEOS
+git clone git://git.saurik.com/ldid.git
+cd ldid
+git submodule update --init
+./make.sh
+cp -f ./ldid $THEOS/bin/ldid
+cd ../
+rm -rf ./ldid
