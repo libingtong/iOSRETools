70b56c4d7967f64502aeeb4bfee1533fad722308
Naville Zhang <zhang.naville@me.com>
2015-12-25 14:05:26 +0000

    Reveal

--- Installer.sh
+++ Installer.sh
@@ -6,8 +6,13 @@ echo "export THEOS=\"/opt/theos\">>~/.bash_profile
 sudo git clone git://github.com/DHowett/theos.git $THEOS
 git clone git://git.saurik.com/ldid.git
 cd ldid
+brew install wget
 git submodule update --init
 ./make.sh
 cp -f ./ldid $THEOS/bin/ldid
 cd ../
 rm -rf ./ldid
+wget http://download.revealapp.com/Reveal.app.zip
+unzip -XK ./Reveal.app.zip
+chmod +x ./Reveal.app/Contents/MacOS/Reveal 
+mv ./Reveal.app /Applications/Reveal.app
