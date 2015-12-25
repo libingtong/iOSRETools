5ce0b9bf75460fb2f24696bb208305f3b1cdf178
Naville Zhang <zhang.naville@me.com>
2015-12-25 14:41:09 +0000

    Hopper

--- Installer.sh
+++ Installer.sh
@@ -23,4 +23,9 @@ wget http://download.revealapp.com/Reveal.app.zip
 unzip -XK ./Reveal.app.zip
 chmod +x ./Reveal.app/Contents/MacOS/Reveal 
 mv ./Reveal.app /Applications/Reveal.app
+echo "Installing Hopper"
+./Hopper.py
+unzip -XK ./Hoppper-Latest.zip
+mv './Hopper Disassembler v3.app' '/Applications/Hopper Disassembler v3.app'
+
 
