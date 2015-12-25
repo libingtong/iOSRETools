2206be49ae0c11f02d1451da718458dac7b3fc14
Naville Zhang <zhang.naville@me.com>
2015-12-25 14:10:02 +0000

    Xcode CommandLine Tools

--- Installer.sh
+++ Installer.sh
@@ -1,18 +1,26 @@
 #/bin/bash
+echo "Installing Xcode Commandline Tools"
+xcode-select --install
 echo "Installing MacPorts"
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+echo "Installing dpkg"
 brew install dpkg
-echo "export THEOS=\"/opt/theos\">>~/.bash_profile
+echo "Installing Theos"
+echo "export THEOS=\"/opt/theos\"" >>~/.bash_profile
 sudo git clone git://github.com/DHowett/theos.git $THEOS
+echo "Installing ldid"
 git clone git://git.saurik.com/ldid.git
 cd ldid
-brew install wget
 git submodule update --init
 ./make.sh
 cp -f ./ldid $THEOS/bin/ldid
 cd ../
 rm -rf ./ldid
+echo "Installing wget"
+brew install wget
+echo "Installing Reveal"
 wget http://download.revealapp.com/Reveal.app.zip
 unzip -XK ./Reveal.app.zip
 chmod +x ./Reveal.app/Contents/MacOS/Reveal 
 mv ./Reveal.app /Applications/Reveal.app
+
